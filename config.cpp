21b13c30321ab9882d363df5d566e023e7414812
Bear altimeters <boris.dureau@neuf.fr>
2019-05-29 08:41:16 +0200

    Add files via upload

---
+++ config.cpp
@@ -0,0 +1,340 @@
+#include "config.h"
+
+//pyro out 1
+const int pyroOut1 = 9;
+int pinApogee = 9;
+
+//pyro out 2
+const int pyroOut2= 10;
+int pinMain = 10;
+
+//pyro out 3
+const int pyroOut3= 5;
+int pinOut3 = 5;
+
+//pyro out 4
+const int pyroOut4=6; 
+int pinOut4 = 6;
+
+int pinOut2 =-1;
+int pinOut1 =-1;
+//int continuityPins[4];
+ConfigStruct config;
+//================================================================
+// read and write in the microcontroler eeprom
+//================================================================
+void defaultConfig()
+{
+  config.unit = 0;
+  config.beepingMode=0;
+  config.outPut1=0;
+  config.outPut2=1;
+  config.outPut3=3;
+  config.outPut1Delay=0;
+  config.outPut2Delay=0;
+  config.outPut3Delay=0;
+  config.mainAltitude=50;
+  config.superSonicYesNo=0;
+  config.beepingFrequency = 440;
+  //config.separationVelocity = 10; 
+  config.nbrOfMeasuresForApogee = 5;
+  config.endRecordAltitude=3;  // stop recording when landing define under which altitude we are not recording
+  config.recordTemperature =0;  //decide if we want to record temperature
+  config.superSonicDelay =0;
+  config.connectionSpeed =38400;
+  config.altimeterResolution = 0; //0 to 4 ie: from low resolution to high
+  config.eepromSize=512;
+  config.noContinuity = 0;
+  config.outPut4=3;
+  config.outPut4Delay=0;
+  config.servo1OnPos=90;
+  config.servo2OnPos=90;
+  config.servo3OnPos=90;
+  config.servo4OnPos=90;
+  config.servo1OffPos=0;
+  config.servo2OffPos=0;
+  config.servo3OffPos=0;
+  config.servo4OffPos=0;
+  config.cksum=0xBA;  
+}
+boolean readAltiConfig() {
+	//set the config to default values so that if any have not been configured we can use the default ones
+	defaultConfig();
+  int i;
+  for( i=0; i< sizeof(config); i++ ) {
+    *((char*)&config + i) = EEPROM.read(CONFIG_START + i);
+  }
+
+  if ( config.cksum != 0xBA ) {
+    return false;
+  }
+
+  return true;
+
+}
+
+/*int getOutPin(int value)
+{
+
+  switch (value)
+  {
+  case 0:
+    pinMain = pyroOut1;
+    break;
+  case 1:
+    pinApogee = pyroOut2;
+    break;
+  case 2:
+    pinOut3 = pyroOut3;
+    break; 
+  default:
+    break;
+  }
+  return 1;
+}*/
+/*
+* write the config received by the console
+*
+*/
+void writeAltiConfig( char *p ) {
+
+  char *str;
+  int i=0;
+  while ((str = strtok_r(p, ",", &p)) != NULL) // delimiter is the comma
+  {
+    SerialCom.println(str);
+    switch (i)
+    {
+    case 1:
+      config.unit =atoi(str);
+      break;
+    case 2:
+      config.beepingMode=atoi(str);
+      break;
+    case 3:
+      config.outPut1=atoi(str);
+      break;   
+    case 4:
+      config.outPut2=atoi(str);
+      break;
+    case 5:
+      config.outPut3=atoi(str);
+      break;
+    case 6:
+      config.mainAltitude=atoi(str);
+      break;
+    case 7:
+      config.superSonicYesNo=atoi(str);
+      break;
+    case 8:
+      config.outPut1Delay=atol(str);
+      break;
+    case 9:
+      config.outPut2Delay=atol(str);
+      break;
+    case 10:
+      config.outPut3Delay=atol(str);
+      break;
+    case 11:
+      config.beepingFrequency =atoi(str);
+      break;
+    case 12:
+      config.nbrOfMeasuresForApogee=atoi(str);
+      break;
+    case 13:
+      config.endRecordAltitude=atol(str);
+      break;
+    case 14:
+      config.recordTemperature=atoi(str);
+      break;
+    case 15:
+      config.superSonicDelay=atoi(str);
+      break;
+    case 16:
+      config.connectionSpeed=atol(str);
+      break;
+    case 17:
+      config.altimeterResolution=atoi(str);
+      break;
+    case 18:
+      config.eepromSize =atoi(str);
+      break;
+    case 19:
+      config.noContinuity=atoi(str);
+      break;
+    
+    case 20:
+      config.outPut4=atoi(str);
+      break;  
+    case 21:
+      config.outPut4Delay=atol(str);
+      break; 
+      
+    case 22:
+      config.servo1OnPos=atoi(str);  
+      break; 
+    case 23:
+      config.servo2OnPos=atoi(str);
+      break;     	
+    case 24:
+      config.servo3OnPos=atoi(str); 
+      break; 
+    case 25:
+      config.servo4OnPos=atoi(str);  
+      break;      
+      
+    case 26:
+      config.servo1OffPos=atoi(str);  
+      break; 
+    case 27:
+      config.servo2OffPos=atoi(str);  
+      break;     
+    case 28:
+      config.servo3OffPos=atoi(str); 
+      break; 
+    case 29:
+      config.servo4OffPos=atoi(str);       
+      break;
+    }
+    i++;
+
+  }
+
+  config.cksum = 0xBA;
+
+  /*for( i=0; i<sizeof(config); i++ ) {
+    EEPROM.write(CONFIG_START+i, *((char*)&config + i));
+  }*/
+  writeConfigStruc();
+}
+//////////////////////////////////////////////////////////////////////////////////////
+void writeConfigStruc()
+{
+    int i;
+    for( i=0; i<sizeof(config); i++ ) {
+      EEPROM.write(CONFIG_START+i, *((char*)&config + i));
+    }
+    SerialCom.print(F("End address: "));
+    SerialCom.print(CONFIG_START+i);
+    SerialCom.print(F("EEPROM length: "));
+    //Serial.print(EEPROM.length());
+}
+
+void printAltiConfig()
+{
+
+  bool ret= readAltiConfig();
+  if(!ret)
+	  SerialCom.print(F("invalid conf"));
+  SerialCom.print(F("$alticonfig"));
+  SerialCom.print(F(","));
+  //Unit
+  SerialCom.print(config.unit);
+  SerialCom.print(F(","));
+  //beepingMode
+  SerialCom.print(config.beepingMode);
+  SerialCom.print(F(","));
+  //output1
+  SerialCom.print(config.outPut1);
+  SerialCom.print(F(","));
+  //output2
+  SerialCom.print(config.outPut2);
+  SerialCom.print(F(","));
+  //output3
+  SerialCom.print(config.outPut3);
+  SerialCom.print(F(","));
+  //supersonicYesNo
+  SerialCom.print(config.superSonicYesNo);
+  SerialCom.print(F(","));
+  //mainAltitude
+  SerialCom.print(config.mainAltitude);
+  SerialCom.print(F(","));
+  //AltimeterName
+  SerialCom.print(F(BOARD_FIRMWARE));
+  SerialCom.print(F(","));
+  //alti major version
+  SerialCom.print(MAJOR_VERSION);
+  //alti minor version
+  SerialCom.print(F(","));
+  SerialCom.print(MINOR_VERSION);
+  SerialCom.print(F(","));
+  //output1 delay
+  SerialCom.print(config.outPut1Delay);
+  SerialCom.print(F(","));
+  //output2 delay
+  SerialCom.print(config.outPut2Delay);
+  SerialCom.print(F(","));
+  //output3 delay
+  SerialCom.print(config.outPut3Delay);
+  SerialCom.print(F(","));
+  //Beeping frequency
+  SerialCom.print(config.beepingFrequency);
+  SerialCom.print(F(","));
+  SerialCom.print(config.nbrOfMeasuresForApogee);
+  SerialCom.print(F(","));
+  SerialCom.print(config.endRecordAltitude);
+  SerialCom.print(F(","));
+  SerialCom.print(config.recordTemperature);
+  SerialCom.print(F(","));
+  SerialCom.print(config.superSonicDelay);
+  SerialCom.print(F(","));
+  SerialCom.print(config.connectionSpeed);
+  SerialCom.print(F(","));
+  SerialCom.print(config.altimeterResolution);
+  SerialCom.print(F(","));
+  SerialCom.print(config.eepromSize);
+  SerialCom.print(F(","));
+  SerialCom.print(config.noContinuity);
+
+  SerialCom.print(F(","));
+  //output4
+  SerialCom.print(config.outPut4);
+  SerialCom.print(F(","));
+   //output4 delay
+  SerialCom.print(config.outPut4Delay);
+  SerialCom.print(F(","));
+  //servo1OnPos
+  SerialCom.print(config.servo1OnPos);
+  SerialCom.print(F(","));
+  //servo2OnPos
+  SerialCom.print(config.servo2OnPos);
+  SerialCom.print(F(","));  
+  //servo3OnPos
+  SerialCom.print(config.servo3OnPos);
+  SerialCom.print(F(","));
+  //servo4OnPos
+  SerialCom.print(config.servo4OnPos);
+  SerialCom.print(F(","));
+
+  //servo1OffPos
+  SerialCom.print(config.servo1OffPos);
+  SerialCom.print(F(","));
+  //servo2OffPos
+  SerialCom.print(config.servo2OffPos);
+  SerialCom.print(F(","));  
+  //servo3OffPos
+  SerialCom.print(config.servo3OffPos);
+  SerialCom.print(F(","));
+  //servo4OffPos
+  SerialCom.print(config.servo4OffPos);
+ 
+  SerialCom.print(F(";\n"));
+}
+bool CheckValideBaudRate(long baudRate)
+{
+	bool valid = false;
+	if(baudRate == 300 ||
+			baudRate == 1200 ||
+			baudRate == 2400 ||
+			baudRate == 4800 ||
+			baudRate == 9600 ||
+			baudRate == 14400 ||
+			baudRate == 19200 ||
+			baudRate == 28800 ||
+			baudRate == 38400 ||
+			baudRate == 57600 ||
+			baudRate == 115200 ||
+			baudRate == 230400)
+	  valid = true;
+	return valid;
+}
---
+++ config.h
@@ -0,0 +1,126 @@
+#ifndef _CONFIG_H
+#define _CONFIG_H
+/*
+ * 
+ * This is where you should do all the configuration
+ * 
+ * First you need to make sure that you have all the require Arduino libraries to compile it
+ * 
+ * required libraries:
+ * Adafruit_BMP085 or BMP280 or BMP085_stm32
+ * avdweb_VirtualDelay
+ */
+
+
+
+/////////////// config changes start here ///////////
+// here choose one of the board that you want to use
+// note that you will need to compile using the Arduino Uno or SMT32 board
+// if you have the original ALTIMULTI board using an ATMega328 define ALTIMULTI
+//#define ALTIMULTI
+
+// if you have a modified ALTIMULTI board using an ATMega328 using different Arduino pins for the
+// pyro output so that they do not fire following a reset of the board then define ALTIMULTIV2
+//#define ALTIMULTIV2
+
+// if you have the STM32 shield then define ALTIMULTISTM32
+//#define ALTIMULTISTM32
+
+// choose the pressure sensor that you are using
+// for most board the pressure sensor is either BMP085 or BMP180 
+// note that BMP085 and 180 are compatible no need to use the new BMP180 library
+#define BMP085_180
+
+// if you have a custom ATMega 328 board using a BMP280 pressure sensor 
+//#define BMP280
+
+// If you want to have additionnal debugging uncomment it
+//#define SERIAL_DEBUG
+#undef SERIAL_DEBUG
+
+
+////////////// config changes end here /////////////
+//////////// do not change anything after unless you know what you are doing /////////////////////
+
+#define MAJOR_VERSION 1
+#define MINOR_VERSION 0
+#define CONFIG_START 32
+
+
+
+
+#define BOARD_FIRMWARE "AltiServo"
+
+#define SerialCom Serial
+
+
+
+
+#include "Arduino.h"
+//used for writing in the microcontroler internal eeprom
+#include <EEPROM.h>
+
+#include "avdweb_VirtualDelay.h"
+
+//pyro out 1
+extern const int pyroOut1;
+extern int pinApogee;
+//pyro out 2
+extern const int pyroOut2;
+extern int pinMain;
+//pyro out 3
+extern const int pyroOut3;
+extern int pinOut3;
+
+//pyro out 4
+extern const int pyroOut4;
+extern int pinOut4;
+
+
+extern int pinOut2;
+extern int pinOut1;
+
+//extern int continuityPins[4];
+
+struct ConfigStruct {
+  int unit;             //0 = meter 1 = feet
+  int beepingMode;      // decide which way you want to report the altitude
+  int outPut1;          // assign a function to each pyro
+  int outPut2;
+  int outPut3;
+  int mainAltitude;     //deployment altitude for the main chute
+  int superSonicYesNo;  // if set to yes do not do any altitude measurement when altimeter starts
+  int outPut1Delay;      // delay output by x ms
+  int outPut2Delay;
+  int outPut3Delay;
+  int beepingFrequency;  // this beeping frequency can be changed
+  int nbrOfMeasuresForApogee; //how many measure to decide that apogee has been reached
+  int endRecordAltitude;  // stop recording when landing define under which altitude we are not recording
+  int recordTemperature;  //decide if we want to record temperature
+  int superSonicDelay;   //nbr of ms during when we ignore any altitude measurements
+  long connectionSpeed;   //altimeter connection baudrate
+  int altimeterResolution; // BMP sensor resolution
+  int eepromSize;
+  int noContinuity;
+  int outPut4;
+  int outPut4Delay;
+  int servo1OnPos;
+  int servo2OnPos;
+  int servo3OnPos;
+  int servo4OnPos;
+  int servo1OffPos;
+  int servo2OffPos;
+  int servo3OffPos;
+  int servo4OffPos;
+  int cksum;  
+};
+extern ConfigStruct config;
+
+extern void defaultConfig();
+extern boolean readAltiConfig();
+extern int getOutPin(int );
+extern void writeAltiConfig( char * );
+extern void printAltiConfig();
+extern void writeConfigStruc();
+extern bool CheckValideBaudRate(long);
+#endif
